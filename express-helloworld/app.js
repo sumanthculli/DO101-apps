8dd4a79c6fb9d86e567cda7bb5e3d0326e16ef2c
sculli <sumanthculli@discover.com>
2022-02-08 10:15:52 -0600

    Add new endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,6 +2,10 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
+  res.send('Hello World!\n');
+});
+
+app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
